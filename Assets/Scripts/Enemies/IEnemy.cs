5e38b57dc394d6e66b58133d75cea5bbcf349c66
SabrinaKatharina <kotzems@hochschule-trier.de>
2016-01-30 04:22:49 +0100

    WanderBehaviour and IEnemy abstract class

---
+++ Assets/Scripts/Enemies/IEnemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ElementTypes{
+
+    Water, Fire, Earth, Shadow
+};
+
+public abstract class IEnemy : MonoBehaviour {
+
+    private int stamina; //health points
+    private ElementTypes elementType;
+    
+    public int Stamina { get; set; }
+    public ElementTypes ElementType {get; set; }
+
+    // LookingAround Behaviour
+    // SeekingBehaviour
+
+    public IEnemy(ElementTypes type) {
+
+        stamina = 100;
+        elementType = type;
+
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public abstract void attackPlayer();
+}
---
+++ Assets/Scripts/Enemies/WanderBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WanderBehaviour : MonoBehaviour {
+
+    private int wanderDirection; // -1 : left ; +1 : right
+    private Vector3 desiredPosition;
+    private float wanderDistance;
+
+	// Use this for initialization
+	void Start () {
+        wanderDirection = 1;
+        setNewDesiredPosition();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(new Vector3( wanderDirection * Time.deltaTime, 0f, 0f));
+           
+        if (Vector3.Distance(desiredPosition, transform.position) >= wanderDistance )
+        {
+            if (wanderDirection == -1) wanderDirection = 1;
+            else if (wanderDirection == 1) wanderDirection = -1;
+            setNewDesiredPosition();
+        }
+	}
+
+    void setNewDesiredPosition() {
+
+        wanderDistance = Random.Range(3f, 5f);
+        desiredPosition = new Vector3(transform.position.x + wanderDirection * wanderDistance, transform.position.y, transform.position.z);
+    }
+
+  
+}
