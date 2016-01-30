9100a3d3299b6193aa96c573e4d90bbec9bde572
barzb <barzb@hochschule-trier.de>
2016-01-30 01:27:23 +0100

    Spell Interface DONE, Spellcaster DOING, SunSpell DOING

---
+++ Assets/Scripts/Abilities/ISpell.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum SpellType 
+{ 
+	SUN,
+	RAIN
+};
+
+// INTERFACE FOR SPELLS
+public abstract class ISpell : MonoBehaviour 
+{
+	// ATTRIBUTES
+	private   float _castRange;
+	private   bool  _isActivated;
+	private   float _activationTime;
+
+	// REFERENCES
+	private List<ISpell> _activeSpells;
+
+	// PROPERTIES
+	public float castRange { get { return _castRange; } }
+	public abstract float duration { get; }
+	public bool isActivated { get { return _isActivated; } }
+
+	// METHODS
+	void FixedUpdate() {
+		if (_isActivated) {
+			// call spell function
+			if (Time.time < _activationTime + duration) {
+				SpellFunction ();
+			}
+			// destroy the spell
+			else {
+				_activeSpells.Remove (this);
+				_isActivated = false;
+				Destroy (this.gameObject);
+			}
+		}
+	}
+
+	public void Activate(List<ISpell> activeSpells, Vector3 targetPosition)
+	{
+		_activeSpells = activeSpells;
+		_activeSpells.Add (this);
+		transform.position = targetPosition;
+		_activationTime = Time.time;
+		_isActivated = true;
+	}
+
+	// override in derived class
+	protected abstract void SpellFunction();
+}
---
+++ Assets/Scripts/Abilities/SunSpell.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunSpell : ISpell {
+
+	public override float duration { get { return 1f; } }
+
+	protected override void SpellFunction()
+	{
+		Debug.DrawLine (Camera.main.ScreenToWorldPoint(new Vector2(Screen.width/2, Screen.height)), transform.position);
+	}
+}
