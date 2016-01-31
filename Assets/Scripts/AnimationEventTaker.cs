2f0909b6352707f80651551616cf47fab611c195
Sushiy <julivizi@yahoo.de>
2016-01-31 09:33:32 +0100

    ik import

---
+++ Assets/Scripts/AnimationEventTaker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationEventTaker : MonoBehaviour
+{
+    CharacterMovement movement;
+
+    void Start()
+    {
+        movement = transform.parent.GetComponent<CharacterMovement>();
+    }
+	public void Jump()
+    {
+        movement.Jump();
+    }
+}
